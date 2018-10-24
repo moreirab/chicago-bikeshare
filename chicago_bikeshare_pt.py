5b1301665bfe16fea15b9641d7326efeb6964051
Bruno Moreira dos Santos <bmds.dev@gmail.com>
2018-10-24 01:24:21 -0300

    TAREFA 3 concluída

--- chicago_bikeshare_pt.py
+++ chicago_bikeshare_pt.py
@@ -53,8 +53,10 @@ input("Aperte Enter para continuar...")
 def column_to_list(data, index):
     column_list = []
     # Dica: Você pode usar um for para iterar sobre as amostras, pegar a feature pelo seu índice, e dar append para uma lista
-    return column_list
+    for line in data:
+        column_list.append(line[index])
 
+    return column_list
 
 # Vamos checar com os gêneros se isso está funcionando (apenas para os primeiros 20)
 print("\nTAREFA 3: Imprimindo a lista de gêneros das primeiras 20 amostras")
